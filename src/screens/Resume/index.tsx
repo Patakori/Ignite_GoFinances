51e7737ba14409834368d742b2846c6a8edbd870
Patakori <dev.edilsonmahmed@gmail.com>
2022-01-26 17:18:41 -0300

    updatedGrapic

---
+++ src/screens/Resume/index.tsx
@@ -0,0 +1,104 @@
+import React, {useEffect, useState} from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage'
+import {VictoryPie} from 'victory-native'
+
+import { HistoryCard } from '../../components/HistoryCard';
+
+import {
+    Container, 
+    Header, 
+    Title,
+    Content,
+
+} from './styles'
+import { categories } from '../../utils/categories';
+import { Category } from '../../components/TransactionCard/styles';
+
+interface TransactionData {
+    id: string;
+    type: 'positive' | 'negative';
+    name: string;
+    amount: string;
+    category: string;
+    date: string;
+}
+
+interface CategoryData {
+    key: string;
+    name: string;
+    total: number;
+    totalFormatted: string;
+    color: string;
+}
+
+export function Resume(){
+    const [totalByCategories, setTotalByCategories] = useState<CategoryData[]>([])
+
+    async function loadData(){
+        const response = await AsyncStorage.getItem('@gofinances:transactions');
+        const responseFormatted = response ? JSON.parse(response) : [];
+
+        const expensives = responseFormatted.filter((expensive: TransactionData)=>expensive.type === 'negative');
+
+        const totalByCategory: CategoryData[] = [];
+
+        categories.forEach(category => {
+            let categorySum = 0;
+
+            expensives.forEach((expensive : TransactionData)=> {
+                if(expensive.category === category.key){
+                    categorySum += Number(expensive.amount)
+                }
+            });
+            
+            if(categorySum > 0){
+                const totalFormatted = categorySum.toLocaleString('pt-BR' , {
+                    style: 'currency',
+                    currency: 'BRL'
+                })
+
+                totalByCategory.push({
+                    key: category.key,
+                    name: category.name,
+                    color: category.color,
+                    total: categorySum,
+                    totalFormatted
+                });
+            }
+        });
+
+
+        setTotalByCategories(totalByCategory);
+    }
+
+    useEffect(()=>{
+        loadData()
+    }, [])
+
+    return(
+        <Container>
+            <Header>
+                <Title> Resumo por categoria </Title>
+            </Header>
+
+            <Content>
+                <VictoryPie 
+                    data={totalByCategories}
+                    x='name'
+                    y='total'
+                />
+                {
+                  totalByCategories.map(item => (
+                      <HistoryCard
+                          key={item.key}
+                          title={item.name}
+                          amount={item.totalFormatted}
+                          color={item.color}
+                      />    
+                  ))
+                }
+            </Content>
+
+        </Container>
+    )
+}
