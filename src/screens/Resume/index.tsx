dfdcb3a7779b7535575ef2f89d3ec44cd7924848
Patakori <dev.edilsonmahmed@gmail.com>
2022-01-28 08:37:26 -0300

    updatedResume

--- src/screens/Resume/index.tsx
+++ src/screens/Resume/index.tsx
@@ -9,10 +9,14 @@ import {
     Header, 
     Title,
     Content,
+    ChartContainer
 
 } from './styles'
 import { categories } from '../../utils/categories';
 import { Category } from '../../components/TransactionCard/styles';
+import { RFValue } from 'react-native-responsive-fontsize';
+
+import { useTheme } from 'styled-components'
 
 interface TransactionData {
     id: string;
@@ -29,16 +33,23 @@ interface CategoryData {
     total: number;
     totalFormatted: string;
     color: string;
+    percent: string;
 }
 
 export function Resume(){
     const [totalByCategories, setTotalByCategories] = useState<CategoryData[]>([])
+
+    const theme = useTheme()
 
     async function loadData(){
         const response = await AsyncStorage.getItem('@gofinances:transactions');
         const responseFormatted = response ? JSON.parse(response) : [];
 
         const expensives = responseFormatted.filter((expensive: TransactionData)=>expensive.type === 'negative');
+
+        const expensivesTotal = expensives.reduce((acumullator: number, expensive:TransactionData)=>{
+            return acumullator + Number(expensive.amount)
+        },0)
 
         const totalByCategory: CategoryData[] = [];
 
@@ -57,12 +68,15 @@ export function Resume(){
                     currency: 'BRL'
                 })
 
+            const percent = `${(categorySum / expensivesTotal * 100).toFixed(0)}%`
+
                 totalByCategory.push({
                     key: category.key,
                     name: category.name,
                     color: category.color,
                     total: categorySum,
-                    totalFormatted
+                    totalFormatted,
+                    percent
                 });
             }
         });
@@ -82,11 +96,22 @@ export function Resume(){
             </Header>
 
             <Content>
-                <VictoryPie 
-                    data={totalByCategories}
-                    x='name'
-                    y='total'
-                />
+                <ChartContainer>
+                    <VictoryPie 
+                        data={totalByCategories}
+                        colorScale={totalByCategories.map(category => category.color)}
+                        style={{
+                            labels: { 
+                                fontSize: RFValue(18),
+                                fontWeight: 'bold',
+                                fill: theme.colors.shape
+                             }
+                        }}
+                        labelRadius={100}
+                        x='percent'
+                        y='total'
+                    />
+                </ChartContainer>
                 {
                   totalByCategories.map(item => (
                       <HistoryCard
